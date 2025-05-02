7c76688f5c29916e7c37717c0cf66f2b1e93163e
lucentrains <lucentrains@gmail.com>
2025-05-02 22:37:24 +0900

    finish 0002_add_two_numbers

--- problems/0002_add_two_numbers/test.py
+++ problems/0002_add_two_numbers/test.py
@@ -25,19 +25,19 @@ class TestAddTwoNumbers(unittest.TestCase):
     def test_example1(self):
         l1 = build_linked_list([2, 4, 3])
         l2 = build_linked_list([5, 6, 4])
-        result = self.solution.add_two_numbers(l1, l2)
+        result = self.solution.addTwoNumbers(l1, l2)
         self.assertEqual(linked_list_to_list(result), [7, 0, 8])
 
     def test_example2(self):
         l1 = build_linked_list([0])
         l2 = build_linked_list([0])
-        result = self.solution.add_two_numbers(l1, l2)
+        result = self.solution.addTwoNumbers(l1, l2)
         self.assertEqual(linked_list_to_list(result), [0])
 
     def test_example3(self):
         l1 = build_linked_list([9, 9, 9, 9, 9, 9, 9])
         l2 = build_linked_list([9, 9, 9, 9])
-        result = self.solution.add_two_numbers(l1, l2)
+        result = self.solution.addTwoNumbers(l1, l2)
         self.assertEqual(linked_list_to_list(result), [8, 9, 9, 9, 0, 0, 0, 1])
 
 if __name__ == '__main__':
