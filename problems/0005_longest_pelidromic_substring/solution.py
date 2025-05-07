702fd7d4e0a126b2de864a968908377edbbb3fc7
lucentrains <lucentrains@gmail.com>
2025-05-07 11:45:17 +0900

    complete 0006 solution.py

--- problems/0005_longest_pelidromic_substring/solution.py
+++ problems/0005_longest_pelidromic_substring/solution.py
@@ -1,8 +1,21 @@
-# Expand Around Center approach
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def check(left: int, right: int) -> str:
-            p_l = left
-            p_r = right
-            while p_l < p_r:
-                
+        def check(i, j):
+            left = i
+            right = j - 1
+
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+
+                left += 1
+                right -= 1
+
+            return True
+
+        for length in range(len(s), 0, -1):
+            for start in range(len(s) - length + 1):
+                if check(start, start + length):
+                    return s[start : start + length]
+
+        return ""
